137aaca415a6293a9cb5d11879eae436cb245888
Barat-Tojiyev <barattojiyev@gmail.com>
2022-09-30 13:45:48 +0500

    dars-1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
+import App from './App';
+import Navbar from './Navbar';
+
 
 
 const root =ReactDom.createRoot(document.getElementById('root'));
 root.render(
  <React.StrictMode>
-<h1>Hello React</h1>
+<App/>
+<Navbar/>
  </React.StrictMode>
   
 
