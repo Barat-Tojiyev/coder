31fd6a2f6326d9a74dbdcf2265eb10452465afa6
Barat-Tojiyev <barattojiyev@gmail.com>
2022-09-30 13:35:24 +0500

    Salom React

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+
+
+const root =ReactDom.createRoot(document.getElementById('root'));
+root.render(
+ <React.StrictMode>
+<h1>Hello React</h1>
+ </React.StrictMode>
+  
+
+)
