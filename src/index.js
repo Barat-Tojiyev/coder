7f6343c654faefbcbdbfbeb35b404fc679783ae2
Barat-Tojiyev <barattojiyev@gmail.com>
2022-10-07 12:00:23 +0500

    dfdg

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDom from 'react-dom/client'
 const root =ReactDom.createRoot(document.getElementById('root'));
 root.render(
  <React.StrictMode>
-<h1>Hello React</h1>
+<h1>Hello Lesson-3</h1>
  </React.StrictMode>
   
 
